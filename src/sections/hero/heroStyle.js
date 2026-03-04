c9d256489bd91cfd00a30f9dd9e074081aab8a2e
veljko_rad <radivojevicveljko@gmail.com>
2026-03-04 10:51:30 +0100

    Fix | on mobile hero image showed

--- src/sections/hero/heroStyle.js
+++ src/sections/hero/heroStyle.js
@@ -1,9 +1,40 @@
-export const heroBg = (theme) =>( { backgroundImage:    theme.palette.mode === 'light'
-    ? "linear-gradient(rgba(255,255,255,0.65), rgba(255,255,255,0.65)), url('/images/crissxcross.png')"
-    : "url('/images/crissxcross.png')", height: '93vh', display: 'flex', position: 'relative' })
+export const heroBg = (theme) => ({
+  backgroundImage:
+    theme.palette.mode === "light"
+      ? "linear-gradient(rgba(255,255,255,0.65), rgba(255,255,255,0.65)), url('/images/crissxcross.png')"
+      : "url('/images/crissxcross.png')",
+  height: "93vh",
+  display: "flex",
+  position: "relative",
+});
 
-export const buttonWork ={ border: '1px solid #3b82f6', width: '145px', color: (theme) => theme.palette.text.primary,textTransform:'none' }
+export const buttonWork = {
+  border: "1px solid #3b82f6",
+  width: "145px",
+  color: (theme) => theme.palette.text.primary,
+  textTransform: "none",
+};
 
-export const heroStack ={ display: { xs: 'none', sm: 'flex' }, position: { xs: 'absolute', lg: 'relative' }, zIndex: '1', maxWidth: { sm: '300px', md: '350px', lg: '600px' }, bottom: { xs: 0, lg: 'auto' }, right: { xs: '15px', lg: 'auto' }, alignSelf: 'flex-end' }
+export const heroStack = {
+  display: "flex",
+  position: { xs: "absolute", lg: "relative" },
+  zIndex: "1",
+  maxWidth: { xs: "180px", sm: "300px", md: "350px", lg: "600px" },
+  bottom: { xs: 0, lg: "auto" },
+  right: { xs: "15px", lg: "auto" },
+  alignSelf: "flex-end",
+};
 
-export const heroImg ={ color: '#3b82f6', width: "63%", position: 'absolute', zIndex: '-1', top: '24%', left: '-6%', animation: 'spin 28s linear infinite', '@keyframes spin': { from: { transform: 'rotate(0deg)' }, to: { transform: 'rotate(360deg)' } } }
+export const heroImg = {
+  color: "#3b82f6",
+  width: "63%",
+  position: "absolute",
+  zIndex: "-1",
+  top: "24%",
+  left: "-6%",
+  animation: "spin 28s linear infinite",
+  "@keyframes spin": {
+    from: { transform: "rotate(0deg)" },
+    to: { transform: "rotate(360deg)" },
+  },
+};
